70939449aaa839d74eb62875f6ced21f6ef7d9ee
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-03-09 13:27:25 -0300

    feat: solving errors

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const http = require('http');
 // const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const { Server } = require('socket.io');
-const Score = require('./models/Score.js');
-const Ip = require('./funcs/ip.js');
+// const Score = require('./models/Score.js');
+// const Ip = require('./funcs/ip.js');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,56 +13,57 @@ const io = new Server(server)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", async(req, res)=>{
-  const ip = await Ip()
-  // res.sendFile(path.join(__dirname + "/views/index.html"))
-  const user = await Score.findOne({
-    where: {
-      ip
-    }
-  })
+  // const ip = await Ip()
+  // // res.sendFile(path.join(__dirname + "/views/index.html"))
+  // const user = await Score.findOne({
+  //   where: {
+  //     ip
+  //   }
+  // })
 
-  if(user === null){
-    res.sendFile(path.join(__dirname + "/views/index.html"))
-  }else{
-    res.redirect("/game")
-  }
+  // if(user === null){
+  //   res.sendFile(path.join(__dirname + "/views/index.html"))
+  // }else{
+  //   res.redirect("/game")
+  // }
+  res.redirect("/game")
 })
-io.on("connection", (socket)=>{
-  console.log("a user connected =>", socket.id)
-  socket.on("disconnect", ()=>{
-    console.log("user disconnected =>", socket.id)
-  })
+// io.on("connection", (socket)=>{
+//   console.log("a user connected =>", socket.id)
+//   socket.on("disconnect", ()=>{
+//     console.log("user disconnected =>", socket.id)
+//   })
   
-  socket.on("login", async(login)=>{
-    const ip = await Ip()
-    console.log(login)
-    const findUser = await Score.findOne({
-      where: {
-        username: login.username,
-        password: login.password
-      }
-    })
-    console.log(findUser)
-    if(findUser === null){
-      await Score.create({
-        username: login.username,
-        password: login.password,
-        ip
-      })
-      socket.emit("message", { message: true })
-    }else{
-      await Score.update({
-        ip
-      }, {
-        where: {
-          username: login.username,
-          password: login.password
-        }
-      })
-      socket.emit("message", { message: true })
-    }
-  })
-})
+//   socket.on("login", async(login)=>{
+//     const ip = await Ip()
+//     console.log(login)
+//     const findUser = await Score.findOne({
+//       where: {
+//         username: login.username,
+//         password: login.password
+//       }
+//     })
+//     console.log(findUser)
+//     if(findUser === null){
+//       await Score.create({
+//         username: login.username,
+//         password: login.password,
+//         ip
+//       })
+//       socket.emit("message", { message: true })
+//     }else{
+//       await Score.update({
+//         ip
+//       }, {
+//         where: {
+//           username: login.username,
+//           password: login.password
+//         }
+//       })
+//       socket.emit("message", { message: true })
+//     }
+//   })
+// })
 
 app.get("/game", async(req, res)=>{
   // res.sendFile(path.join(__dirname + "/views/game.html"))
